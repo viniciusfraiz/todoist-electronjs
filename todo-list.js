5a67dde8e5a7eee47563ff867a240cfb1d550ec2
vinicus fraiz <viniciusfraiz10@gmail.com>
2022-04-20 22:32:30 -0300

    adicionando botao de enter

--- todo-list.js
+++ todo-list.js
@@ -1,5 +1,6 @@
-function adicionarItem() {
-    const valorInput = "Minha Primeira tarefa";
+function adicionarItem(event) {
+    if(event && event.keyCode !== 13) return; 
+    const valorInput = document.getElementById("").value
 
     //CRIANDO A TAG UL DO NOSSO HTML PELO ID
     const minhaTagUL = document.getElementById("Lista-de-tarefas");
