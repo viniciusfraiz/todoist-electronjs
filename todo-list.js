619ef940b873977e19092b865d0850ae08a94c70
vinicus fraiz <viniciusfraiz10@gmail.com>
2022-04-26 21:41:03 -0300

    adicionando icone de remover

--- todo-list.js
+++ todo-list.js
@@ -1,10 +1,10 @@
 function adicionarItem(event) {
+    
+    console.log(event)
     const tecla = event.key;
 
- 
-    if(tecla !== "Enter") return;
+    if(tecla && tecla !== "Enter") return;
 
-    console.log(tecla)
 
     const valorInput = document.getElementById("input").value
 
@@ -15,25 +15,30 @@ function adicionarItem(event) {
     const criarTagLI = document.createElement("li") ; 
 
     //Criando uma tag em negrito
-        const tagRemover = "<b onclick='removerItem(event)'>Remover</b>" ; 
+        const tagRemover = `<i onclick='removerItem(event)' class="fa-solid fa-circle-minus"></i>`; 
 
     //Adicionando um texto  para nossa tag li criada
     criarTagLI.innerHTML = valorInput + tagRemover ; 
 
-    // ADICIONANDO A TAG Li para Nossa Ul
+    // Adiconando A tag Li para Nossa Ul
     minhaTagUL.appendChild(criarTagLI) ;
     
 
    console.log("ENTROU NA FUNÇAO ADICIONA ITEM")
 
-
 }
     
 function removerItem(event) {
-    //PEGANDO <B> DENTRO DO NOSSO EVENTO
-    const tagRemover = event.target; 
 
-    console.log("CLIKEI NA FUNÇAO REMOVER") 
 
-    console.log(tagRemover)
+
+    //PEGANDO <i> DENTRO DO NOSSO EVENTO
+    const  meuIcone = event.target; 
+
+    //PEGADNO A TAG <LI> ATRAVÉS DA TAG <I> COM PROPRIEDADE PARENTELEMENT
+    const minhaLi = meuIcone.parentElement
+    
+    minhaLi.remove()
+
+    
 }
